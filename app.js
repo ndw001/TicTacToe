4238d6331b395c91ea0099709df72c77a0e65185
Nathan Wong <natdanwongielhan@yahoo.com>
2021-01-06 16:45:31 -0800

    No repeat button works now, but reset button stopped working. will work on that now

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ positions.X = {};
 positions.O = {};
 positions.all = [];
 let playerXO = ["One", "X"];
+let found = false;
 
 let updatePositions = (string, object) => {
   let letters = string.split(",");
@@ -54,51 +55,150 @@ let checkWinner = (player, object) => {
 document.getElementById('playerTurn').innerHTML = `It's Player ${playerXO[0]}'s turn.`;
 
 document.getElementById('1LA').addEventListener("click", function(){
-  document.getElementById('1LA').innerHTML = `[${playerXO[1]}]`;
-  updatePositions('1,L,A', positions[`${playerXO[1]}`]);
-  checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '1LA'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('1LA');
+    document.getElementById('1LA').innerHTML = `[${playerXO[1]}]`;
+    updatePositions('1,L,A', positions[`${playerXO[1]}`]);
+    checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
+
+
 document.getElementById('1B').addEventListener("click", function(){
-  document.getElementById('1B').innerHTML = `[${playerXO[1]}]`;
-  updatePositions('1,B', positions[`${playerXO[1]}`]);
-  checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '1B'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('1B');
+    document.getElementById('1B').innerHTML = `[${playerXO[1]}]`;
+    updatePositions('1,B', positions[`${playerXO[1]}`]);
+    checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
+
+
+
 document.getElementById('1RC').addEventListener("click", function(){
-  document.getElementById('1RC').innerHTML = `[${playerXO[1]}]`;
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '1RC'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('1RC');
+    document.getElementById('1RC').innerHTML = `[${playerXO[1]}]`;
   updatePositions('1,R,C', positions[`${playerXO[1]}`]);
   checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
 
 document.getElementById('2A').addEventListener("click", function(){
-  document.getElementById('2A').innerHTML = `[${playerXO[1]}]`;
-  updatePositions('2,A', positions[`${playerXO[1]}`]);
-  checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '2A'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('2A');
+    document.getElementById('2A').innerHTML = `[${playerXO[1]}]`;
+    updatePositions('2,A', positions[`${playerXO[1]}`]);
+    checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
 document.getElementById('2LRB').addEventListener("click", function(){
-  document.getElementById('2LRB').innerHTML = `[${playerXO[1]}]`;
-  updatePositions('2,L,R,B', positions[`${playerXO[1]}`]);
-  checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '2LRB'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('2LRB');
+    document.getElementById('2LRB').innerHTML = `[${playerXO[1]}]`;
+    updatePositions('2,L,R,B', positions[`${playerXO[1]}`]);
+    checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
 document.getElementById('2C').addEventListener("click", function(){
-  document.getElementById('2C').innerHTML = `[${playerXO[1]}]`;
-  updatePositions('2,C', positions[`${playerXO[1]}`]);
-  checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '2C'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('2C');
+    document.getElementById('2C').innerHTML = `[${playerXO[1]}]`;
+    updatePositions('2,C', positions[`${playerXO[1]}`]);
+    checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
 
 document.getElementById('3RA').addEventListener("click", function(){
-  document.getElementById('3RA').innerHTML = `[${playerXO[1]}]`;
-  updatePositions('3,R,A', positions[`${playerXO[1]}`]);
-  checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '3RA'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('3RA');
+    document.getElementById('3RA').innerHTML = `[${playerXO[1]}]`;
+    updatePositions('3,R,A', positions[`${playerXO[1]}`]);
+    checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
+
+
 document.getElementById('3B').addEventListener("click", function(){
-  document.getElementById('3B').innerHTML = `[${playerXO[1]}]`;
-  updatePositions('3,B', positions[`${playerXO[1]}`]);
-  checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '3B'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('3B');
+    document.getElementById('3B').innerHTML = `[${playerXO[1]}]`;
+    updatePositions('3,B', positions[`${playerXO[1]}`]);
+    checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
+
+
 document.getElementById('3LC').addEventListener("click", function(){
-  document.getElementById('3LC').innerHTML = `[${playerXO[1]}]`;
-  updatePositions('3,L,C', positions[`${playerXO[1]}`]);
-  checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  for(let z = 0; z < positions.all.length; z++){
+    if(positions.all[z] === '3LC'){
+      found = true;
+    }
+  }
+  if(!found){
+    positions.all.push('3LC');
+    document.getElementById('3LC').innerHTML = `[${playerXO[1]}]`;
+    updatePositions('3,L,C', positions[`${playerXO[1]}`]);
+    checkWinner(`${playerXO[0]}`, positions[`${playerXO[1]}`]);
+  } else {
+    found = false;
+  }
 })
 
 document.getElementById('reset').addEventListener("click", function(){
